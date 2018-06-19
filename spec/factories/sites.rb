a3b9a8275cd754a124df687150f365dc818349c9
Alec Sanger <sanger.alec@gmail.com>
2018-06-19 12:48:04 -0400

    test(sites): Adds factorygirl and faker gems, adds model tests for sites

---
+++ spec/factories/sites.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :site do
+    url "http://www.someurl.com"
+  end
+end
--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Site, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryBot.create(:site)).to be_valid
+  end
+  it "must have a URL" do
+    expect(FactoryBot.build(:site, url: nil)).to_not be_valid
+  end
 end
