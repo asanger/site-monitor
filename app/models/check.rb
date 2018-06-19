becac63fb835a3f1a72d6bfe6c85fefb83db58bf
Alec Sanger <sanger.alec@gmail.com>
2018-06-19 13:54:33 -0400

    feat(checks): Adds model validations

--- app/models/check.rb
+++ app/models/check.rb
@@ -1,3 +1,7 @@
 class Check < ApplicationRecord
   belongs_to :site
+
+  validates :status_code, presence: true, format: { with: /\b[0-9]{3}\b/ }
+  validates :response_time, presence: true, numericality: { only_integer: true }
+
 end
