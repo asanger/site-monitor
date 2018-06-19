fcf6010a509e3ca00bc65523dfed816f8df3e4a1
Alec Sanger <sanger.alec@gmail.com>
2018-06-19 13:57:52 -0400

    test(checks): Adds model tests

--- spec/models/check_spec.rb
+++ spec/models/check_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Check, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryBot.create(:site)).to be_valid
+  end
+  it "has a status code" do
+    expect(FactoryBot.build(:check, status_code: nil)).to_not be_valid
+  end
+  it "has a response time" do
+    expect(FactoryBot.build(:check, response_time: nil)).to_not be_valid
+  end
 end
